fc86ad0ae79ebc4090614c66c790f975eb1b636a
Andrew Cooper <andrew.cooper3@citrix.com>
2016-02-18 15:30:16 +0100

    x86: fix unintended fallthrough case from XSA-154
    ... and annotate the other deliberate one: Coverity objects otherwise.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    
    One of the two instances was actually a bug.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    master commit: 8dd6d1c099865ee5f5916616a0ca79cd943c46f9
    master date: 2016-02-18 15:10:07 +0100
    
    (cherry picked from commit ccc7adf9cff5d5f93720afcc1d0f7227d50feab2)
    Signed-off-by: Bastian Blank <bastian.blank@credativ.de>

--- xen/arch/x86/mm.c
+++ xen/arch/x86/mm.c
@@ -864,9 +864,11 @@ get_page_from_l1e(
             case 0:
                 break;
             case 1:
-                if ( IS_PRIV(l1e_owner) )
+                if ( !IS_PRIV(l1e_owner) )
+                    break;
+                /* fallthrough */
             case -1:
-                    return 0;
+                return 0;
             default:
                 ASSERT(0);
             }
