7d00cf36f360409c5cd41ebed295e8c51d0e2f03
Jan Beulich <jbeulich@suse.com>
2016-03-04 14:09:04 +0100

    x86: use "unsigned int" for cache attribute values
    ... where suitable. But note that the type of "cacheattr" in
    get_page_from_l1e() specifically needs to remain "unsigned long".
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/include/asm-x86/page.h
+++ xen/include/asm-x86/page.h
@@ -347,11 +347,11 @@ l1_pgentry_t *virt_to_xen_l1e(unsigned long v);
 extern void set_pdx_range(unsigned long smfn, unsigned long emfn);
 
 /* Convert between PAT/PCD/PWT embedded in PTE flags and 3-bit cacheattr. */
-static inline uint32_t pte_flags_to_cacheattr(uint32_t flags)
+static inline unsigned int pte_flags_to_cacheattr(unsigned int flags)
 {
     return ((flags >> 5) & 4) | ((flags >> 3) & 3);
 }
-static inline uint32_t cacheattr_to_pte_flags(uint32_t cacheattr)
+static inline unsigned int cacheattr_to_pte_flags(unsigned int cacheattr)
 {
     return ((cacheattr & 4) << 5) | ((cacheattr & 3) << 3);
 }
