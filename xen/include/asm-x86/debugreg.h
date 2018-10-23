e0e40bd592554ebaae69554680e9db94f59e5c29
Peter Dreuw <peter.dreuw@credativ.de>
2018-10-23 11:25:05 +0200

    some definitions necessary for xsa260/265 patch to build on Xen4.4

--- xen/include/asm-x86/debugreg.h
+++ xen/include/asm-x86/debugreg.h
@@ -20,6 +20,11 @@
 #define DR_TRAP3        (0x8)           /* db3 */
 #define DR_STEP         (0x4000)        /* single-step */
 #define DR_SWITCH       (0x8000)        /* task switch */
+#define DR_NOT_RTM      (0x10000)       /* clear: #BP inside RTM region */
+#define DR_STATUS_RESERVED_ZERO (~0xffffeffful) /* Reserved, read as zero */
+#define DR_STATUS_RESERVED_ONE  0xffff0ff0ul /* Reserved, read as one */
+
+#define X86_DR6_DEFAULT 0xffff0ff0ul    /* Default %dr6 value. */
 
 /* Now define a bunch of things for manipulating the control register.
    The top two bytes of the control register consist of 4 fields of 4
@@ -62,6 +67,7 @@
 #define DR_CONTROL_RESERVED_ONE  (0x00000400ul) /* Reserved, read as one */
 #define DR_LOCAL_EXACT_ENABLE    (0x00000100ul) /* Local exact enable */
 #define DR_GLOBAL_EXACT_ENABLE   (0x00000200ul) /* Global exact enable */
+#define DR_RTM_ENABLE            (0x00000800ul) /* RTM debugging enable */
 #define DR_GENERAL_DETECT        (0x00002000ul) /* General detect enable */
 
 #endif /* _X86_DEBUGREG_H */
